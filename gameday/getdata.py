b3b85d6f5b12e5caefd4e455bd26034509b810ee
jeffabc1997 <jeff.tsai86@gmail.com>
2023-09-26 11:54:12 +0800

    add A team Name to FangraphsID update team batting stats also on html html has more info of pitcher Add buttons

--- gameday/getdata.py
+++ gameday/getdata.py
@@ -4,14 +4,23 @@ from datetime import datetime, timezone, timedelta
 
 # Should put this function else where
 # Add abbreviation name for teams
-teamAbbrev = {'Arizona Diamondbacks': 'ARI', 'Chicago Cubs': 'CHC', 'St. Louis Cardinals': 'STL', 'Cincinnati Reds': 'CIN', 'Seattle Mariners': 'SEA', 'Tampa Bay Rays': 'TB', 'Chicago White Sox': 'CWS', 'Detroit Tigers': 'DET', 'Los Angeles Dodgers': 'LAD', 'Washington Nationals': 'WSH',\
-           'Miami Marlins': 'MIA', 'Philadelphia Phillies': 'PHI', 'Milwaukee Brewers': 'MIL', 'New York Yankees': 'NYY', 'Kansas City Royals': 'KC', 'Toronto Blue Jays': 'TOR', 'Baltimore Orioles': 'BAL', 'Boston Red Sox': 'BOS', \
-           'Pittsburgh Pirates': 'PIT', 'Atlanta Braves': 'ATL', 'Oakland Athletics': 'OAK', 'Texas Rangers': 'TEX', 'San Diego Padres': 'SD', 'Houston Astros': 'HOU', 'New York Mets': 'NYM', 'Minnesota Twins': 'MIN', 'Cleveland Guardians': 'CLE', 'Los Angeles Angels': 'LAA',\
-           'Colorado Rockies': 'COL', 'San Francisco Giants': 'SF'}
+teamAbbrev = {'Arizona Diamondbacks': 'ARI', 'Atlanta Braves': 'ATL', 'Baltimore Orioles': 'BAL', 'Boston Red Sox': 'BOS', 'Chicago Cubs': 'CHC', 'Cincinnati Reds': 'CIN', \
+              'Cleveland Guardians': 'CLE', 'Colorado Rockies': 'COL', 'Chicago White Sox': 'CWS', 'Detroit Tigers': 'DET', 'Houston Astros': 'HOU', 'Kansas City Royals': 'KC', \
+              'Los Angeles Angels': 'LAA', 'Los Angeles Dodgers': 'LAD', 'Miami Marlins': 'MIA', 'Milwaukee Brewers': 'MIL', 'Minnesota Twins': 'MIN', 'New York Mets': 'NYM', \
+              'New York Yankees': 'NYY', 'Oakland Athletics': 'OAK', 'Philadelphia Phillies': 'PHI', 'Pittsburgh Pirates': 'PIT', 'San Diego Padres': 'SD', 'Seattle Mariners': 'SEA', \
+              'San Francisco Giants': 'SF', 'St. Louis Cardinals': 'STL', 'Tampa Bay Rays': 'TB', 'Texas Rangers': 'TEX', 'Toronto Blue Jays': 'TOR', 'Washington Nationals': 'WSH'}
+teamtoFgId = {'Arizona Diamondbacks': 15, 'Atlanta Braves': 16, 'Baltimore Orioles': 2, 'Boston Red Sox': 3, 'Chicago Cubs': 17, 'Cincinnati Reds': 18, \
+              'Cleveland Guardians': 5, 'Colorado Rockies': 19, 'Chicago White Sox': 4, 'Detroit Tigers': 6, 'Houston Astros': 21, 'Kansas City Royals': 7, \
+              'Los Angeles Angels': 1, 'Los Angeles Dodgers': 22, 'Miami Marlins': 20, 'Milwaukee Brewers': 23, 'Minnesota Twins': 8, 'New York Mets': 25, \
+              'New York Yankees': 9, 'Oakland Athletics': 10, 'Philadelphia Phillies': 26, 'Pittsburgh Pirates': 27, 'San Diego Padres': 29, 'Seattle Mariners': 11, \
+              'San Francisco Giants': 30, 'St. Louis Cardinals': 28, 'Tampa Bay Rays': 12, 'Texas Rangers': 13, 'Toronto Blue Jays': 14, 'Washington Nationals': 24}
 
+allPitcherStats = pybaseball.pitching_stats(2023, qual = 1)
+allTeamBatStats = pybaseball.team_batting(2023, 2023, ind=0).dropna(axis=1)
 def findpitcher(mlbids):
     statlist = []
-    df = pybaseball.pitching_stats(2023, qual = 1) # all pitching stats
+    global allPitcherStats
+    # df = pybaseball.pitching_stats(2023, qual = 1) # all pitching stats
     # put only 1 player in this function for the right order
     for mid in mlbids:
         key = pybaseball.playerid_reverse_lookup([mid], key_type='mlbam')
@@ -21,15 +30,22 @@ def findpitcher(mlbids):
             print(f"MLB player ID {mid} not found.")
             statlist.append({})
             continue
-        aPlayer = df.loc[df['IDfg'] == fgid]
+        aPlayer = allPitcherStats.loc[allPitcherStats['IDfg'] == fgid]
         if aPlayer.empty:
             print(f"fgID: {fgid}, not a qualified pitcher.")
             statlist.append({})
             continue
         else: # customize the information we want to display on webpage
-            statlist.append({'Win': aPlayer['W'].values[0], 'Loss': aPlayer['L'].values[0], 'IP': aPlayer['IP'].values[0], 'ERA': aPlayer['ERA'].values[0],'xFIP-': aPlayer['xFIP-'].values[0], 'xERA': aPlayer['xERA'].values[0]})
+            statlist.append({'Win': aPlayer['W'].values[0], 'Loss': aPlayer['L'].values[0], \
+                             'IP': aPlayer['IP'].values[0], 'ERA': aPlayer['ERA'].values[0], \
+                             'xFIPm': aPlayer['xFIP-'].values[0], 'xERA': aPlayer['xERA'].values[0]}) # beware that xFIP- is renamed to xFIPm
 
     return statlist
+def findTeamStats(name):
+    global allTeamBatStats, teamtoFgId
+    battingOfTeam = allTeamBatStats.loc[allTeamBatStats['teamIDfg'] == teamtoFgId[name], ["wRC+", "Hard%+", "wOBA"]]
+    print(battingOfTeam)
+    return battingOfTeam.to_dict('records')[0]
 # getting the year from the current date and time
 current_datetime = datetime.now(timezone.utc) - timedelta(hours=8) # Time zone: western time in US
 date = current_datetime.strftime("%m/%d/%Y")
@@ -42,6 +58,7 @@ schedule = statsapi.get("schedule", params)
 gamethatday = schedule["dates"][0]["games"]
 homeids = []
 awayids =[]
+
 for g in gamethatday: # get starting pitcher ID
     try:
         homeids.append(g['teams']['home']['probablePitcher']['id'])
@@ -56,22 +73,24 @@ for g in gamethatday: # get starting pitcher ID
         print("away probablePitcher Key not found")
         awayids.append(-1)
 
-homestats = findpitcher(homeids) # get a list of dictionaries
-awaystats = findpitcher(awayids)
+homePitcherStats = findpitcher(homeids) # get a list of dictionaries
+awayPitcherStats = findpitcher(awayids)
 
-for i in range(len(homestats)): # update data in gamethatday
+for i in range(len(homePitcherStats)): # update data in gamethatday
     try:
-        gamethatday[i]['teams']['home']['probablePitcher'].update(homestats[i])
+        gamethatday[i]['teams']['home']['probablePitcher'].update(homePitcherStats[i])
     except:
-        # should add probablePitcher dict for home
-        gamethatday[i]['teams']['home']['probablePitcher'] = {'fullName' : 'TBD'}
-        # pass just now
+        # if can't find a pitcher, give it TBD
+        gamethatday[i]['teams']['home']['probablePitcher'] = {'fullName' : 'TBD'} # STILL NEED TO CHECK
+
     try:
-        gamethatday[i]['teams']['away']['probablePitcher'].update(awaystats[i]) # update global dict
+        gamethatday[i]['teams']['away']['probablePitcher'].update(awayPitcherStats[i]) # update global dict
     except:
-        gamethatday[i]['teams']['away']['probablePitcher'] = {'fullName' : 'TBD'}
+        gamethatday[i]['teams']['away']['probablePitcher'] = {'fullName' : 'TBD'} 
 
     gamethatday[i]['teams']['home']['team_abbrev'] = teamAbbrev[gamethatday[i]['teams']['home']['team']['name']] # give a abbreviation name
     gamethatday[i]['teams']['away']['team_abbrev'] = teamAbbrev[gamethatday[i]['teams']['away']['team']['name']]
 
-# print(gamethatday)
+    gamethatday[i]['teams']['home'].update(findTeamStats(gamethatday[i]['teams']['home']['team']['name'])) # update the team's batting stats
+    gamethatday[i]['teams']['away'].update(findTeamStats(gamethatday[i]['teams']['away']['team']['name']))
+print(gamethatday)
