7738fa03a00ad5206d4987c1f20282e0379dc470
jeffabc1997 <jeff.tsai86@gmail.com>
2024-05-29 00:15:29 +0800

    feat: insert data into model max_digits must > decimal places it's recommended to round the floating number

--- gameday/getdata.py
+++ gameday/getdata.py
@@ -2,6 +2,7 @@ import statsapi
 import pybaseball
 from datetime import datetime, timezone, timedelta
 import math
+from .models import Game, Team, Pitcher
 # Should put this function else where
 # Add abbreviation name for teams
 teamAbbrev = {'Arizona Diamondbacks': 'ARI', 'Atlanta Braves': 'ATL', 'Baltimore Orioles': 'BAL', 'Boston Red Sox': 'BOS', 'Chicago Cubs': 'CHC', 'Cincinnati Reds': 'CIN', \
@@ -15,8 +16,8 @@ teamtoFgId = {'Arizona Diamondbacks': 15, 'Atlanta Braves': 16, 'Baltimore Oriol
               'New York Yankees': 9, 'Oakland Athletics': 10, 'Philadelphia Phillies': 26, 'Pittsburgh Pirates': 27, 'San Diego Padres': 29, 'Seattle Mariners': 11, \
               'San Francisco Giants': 30, 'St. Louis Cardinals': 28, 'Tampa Bay Rays': 12, 'Texas Rangers': 13, 'Toronto Blue Jays': 14, 'Washington Nationals': 24}
 
-thisYear = 2023
-baseballRefPitcher = pybaseball.pitching_stats_bref(2023)
+thisYear = 2024
+baseballRefPitcher = pybaseball.pitching_stats_bref(thisYear)
 allPitcherStats = pybaseball.pitching_stats(thisYear, qual = 1)
 allTeamBatStats = pybaseball.team_batting(thisYear, thisYear, ind=0)
 def findpitcher(mlbid):
@@ -30,10 +31,10 @@ def findpitcher(mlbid):
         brefPlayer = baseballRefPitcher.loc[baseballRefPitcher['mlbID'] == str(mlbid)].to_dict('records')[0]
         statDict['Win'] = int(brefPlayer['W']) if not math.isnan(brefPlayer['W']) else 0
         statDict['Loss'] = int(brefPlayer['L']) if not math.isnan(brefPlayer['L']) else 0
-        statDict['IP'] = brefPlayer['IP'] if not math.isnan(brefPlayer['IP']) else 0
-        statDict['ERA'] = "{:.2f}".format(brefPlayer['ERA'])
-        statDict['WHIP'] = "{:.2f}".format(brefPlayer['WHIP'])
-        statDict['SO9'] = "{:.2f}".format(brefPlayer['SO9'])
+        statDict['IP'] = round(brefPlayer['IP'],2) if not math.isnan(brefPlayer['IP']) else 0
+        statDict['ERA'] = round(brefPlayer['ERA'], 2)# "{:.2f}".format(brefPlayer['ERA'])
+        statDict['WHIP'] = round(brefPlayer['WHIP'],2)# "{:.2f}".format(brefPlayer['WHIP'])
+        # statDict['SO9'] = round(brefPlayer['SO9'], 2)#"{:.2f}".format(brefPlayer['SO9'])
     except:
         brefOK = False # we can try these data in Fangraph
         print(f"No data in Baseball Reference. MLB ID: {mlbid}")
@@ -45,10 +46,10 @@ def findpitcher(mlbid):
         if brefOK == False:
             statDict['Win'] = aPlayer['W']
             statDict['Loss'] = aPlayer['L']
-            statDict['IP'] = aPlayer['IP']
-            statDict['ERA'] = "{:.2f}".format(aPlayer['ERA'])
+            statDict['IP'] = round(aPlayer['IP'], 2)
+            statDict['ERA'] = round(aPlayer['ERA'], 2)# "{:.2f}".format(aPlayer['ERA'])
         statDict['xFIPm'] = aPlayer['xFIP-'] # beware that xFIP- is renamed to xFIPm
-        statDict['xERA'] = aPlayer['xERA']
+        statDict['xERA'] = round(aPlayer['xERA'], 2)
     except:
         print(f"MLB ID:{mlbid} not found in Fangraph API.")
         
@@ -64,11 +65,17 @@ def findTeamStats(name):
     battingOfTeam = allTeamBatStats.loc[allTeamBatStats['teamIDfg'] == teamtoFgId[name], ["OPS", "wOBA", "wRC+"]]
     battingOfTeam = battingOfTeam.rename(columns={'wRC+': 'wRCp'}) # html can't parse '+'
     battingdict = battingOfTeam.to_dict('records')[0]
-    battingdict['wOBA'] = "{:.3f}".format(battingdict['wOBA'])
-    battingdict['OPS'] = "{:.3f}".format(battingdict['OPS'])
+    battingdict['wOBA'] = round(battingdict['wOBA'], 3) #"{:.3f}".format(battingdict['wOBA'])
+    battingdict['OPS'] = round(battingdict['OPS'], 3) #"{:.3f}".format(battingdict['OPS'])
     return battingdict
 # getting the year from the current date and time
 def mlbgame():
+    obj_tbd, created = Pitcher.objects.update_or_create(
+            id=-1,
+            defaults={
+                'fullName': 'TBD',
+            }
+        )
     current_datetime = datetime.now() # Time zone: western time in US
     date = current_datetime.strftime("%m/%d/%Y")
     # date = "09/30/2023"
@@ -80,7 +87,8 @@ def mlbgame():
     schedule = statsapi.get("schedule", params) # Future Progress: can update schedule for probable pitcher without process termination
     if schedule['totalGames'] == 0:
         return {}
-    gamethatday = schedule["dates"][0]["games"] 
+    gamethatday = schedule["dates"][0]["games"]
+    # print(gamethatday)
     homeids = []
     awayids =[]
 
@@ -103,21 +111,115 @@ def mlbgame():
 
     for i in range(len(gamethatday)): # update data in gamethatday
         try:
-            gamethatday[i]['teams']['home']['probablePitcher'].update(findpitcher(gamethatday[i]['teams']['home']['probablePitcher']['id']))
+            home_pitcher_dic = findpitcher(gamethatday[i]['teams']['home']['probablePitcher']['id'])
+            home_pitcher_dic['id'] = gamethatday[i]['teams']['home']['probablePitcher']['id']
+            home_pitcher_dic['fullName'] = gamethatday[i]['teams']['home']['probablePitcher']['fullName']
+            print('home pitcher', home_pitcher_dic)
+            gamethatday[i]['teams']['home']['probablePitcher'].update(home_pitcher_dic)
         except:
             # if can't find a pitcher, give it TBD
             gamethatday[i]['teams']['home']['probablePitcher'] = {'fullName' : 'TBD'} # STILL NEED TO CHECK
-
+            home_pitcher_dic['id'] = -1
+            home_pitcher_dic['fullName'] = 'TBD'
         try:
-            gamethatday[i]['teams']['away']['probablePitcher'].update(findpitcher(gamethatday[i]['teams']['away']['probablePitcher']['id'])) # update global dict
+            
+            away_pitcher_dic = findpitcher(gamethatday[i]['teams']['away']['probablePitcher']['id'])
+            away_pitcher_dic['id'] = gamethatday[i]['teams']['away']['probablePitcher']['id']
+            away_pitcher_dic['fullName'] = gamethatday[i]['teams']['away']['probablePitcher']['fullName']
+            gamethatday[i]['teams']['away']['probablePitcher'].update(away_pitcher_dic) # update global dict
         except:
             gamethatday[i]['teams']['away']['probablePitcher'] = {'fullName' : 'TBD'} 
+            away_pitcher_dic['id'] = -1
+            away_pitcher_dic['fullName'] = 'TBD'
+        home_team_dic = {'team_abbrev': teamAbbrev[gamethatday[i]['teams']['home']['team']['name']], 'team_id': gamethatday[i]['teams']['home']['team']['id'], 
+                         'team_name': gamethatday[i]['teams']['home']['team']['name'], 'win': gamethatday[i]['teams']['home']['leagueRecord']['wins'], 
+                         'loss': gamethatday[i]['teams']['home']['leagueRecord']['losses'], 'win_pct': gamethatday[i]['teams']['home']['leagueRecord']['pct']}
+        away_team_dic = {'team_abbrev': teamAbbrev[gamethatday[i]['teams']['away']['team']['name']], 'team_id': gamethatday[i]['teams']['away']['team']['id'], 
+                         'team_name': gamethatday[i]['teams']['away']['team']['name'], 'win': gamethatday[i]['teams']['away']['leagueRecord']['wins'],
+                         'loss': gamethatday[i]['teams']['away']['leagueRecord']['losses'], 'win_pct': gamethatday[i]['teams']['away']['leagueRecord']['pct']}
+        try:
+            home_team_dic['score'] = gamethatday[i]['teams']['home']['score']
+        except:
+            home_team_dic['score'] = 0
+        try:
+            away_team_dic['score'] = gamethatday[i]['teams']['away']['score']
+        except:
+            away_team_dic['score'] = 0
+        home_team_dic.update(findTeamStats(home_team_dic['team_name'])) # update the team's batting stats
+        away_team_dic.update(findTeamStats(away_team_dic['team_name']))
+        print('home team dic', home_team_dic)
+        print('away team dic', away_team_dic)
+        print('home pitcher dic', home_pitcher_dic)
+        print('away pitcher dic', away_pitcher_dic)
 
         gamethatday[i]['teams']['home']['team_abbrev'] = teamAbbrev[gamethatday[i]['teams']['home']['team']['name']] # give a abbreviation name
         gamethatday[i]['teams']['away']['team_abbrev'] = teamAbbrev[gamethatday[i]['teams']['away']['team']['name']]
 
         gamethatday[i]['teams']['home'].update(findTeamStats(gamethatday[i]['teams']['home']['team']['name'])) # update the team's batting stats
         gamethatday[i]['teams']['away'].update(findTeamStats(gamethatday[i]['teams']['away']['team']['name']))
-    print("in getdata: ",gamethatday)
+
+        # if(gamethatday[i]['teams']['away']['probablePitcher']['fullName'] == 'TBD'):
+        #     gamethatday[i]['teams']['home']['probablePitcher']['id'] = -1
+        # h_pitcher_id = gamethatday[i]['teams']['home']['probablePitcher']['id'] if gamethatday[i]['teams']['home']['probablePitcher']['fullName'] != 'TBD' else -1
+        # a_pitcher_id = gamethatday[i]['teams']['away']['probablePitcher']['id'] if gamethatday[i]['teams']['away']['probablePitcher']['fullName'] != 'TBD' else -1
+        # if(gamethatday[i]['teams']['home']['probablePitcher']['fullName'] == 'TBD'):
+        #     gamethatday[i]['teams']['away']['probablePitcher']['id'] = -1
+        try:
+            obj_home_pitcher, created = Pitcher.objects.update_or_create(
+                id=home_pitcher_dic['id'],
+                defaults={
+                    'fullName': home_pitcher_dic['fullName'],'IP': home_pitcher_dic['IP'],
+                    'Win': home_pitcher_dic['Win'], 'Loss': home_pitcher_dic['Loss'], #'ERA': home_pitcher_dic['ERA'],
+                    # 'xERA': home_pitcher_dic['xERA'], 'xFIPm': home_pitcher_dic['xFIPm']
+                }
+            )
+        except:
+            print('home not created',obj_home_pitcher)
+        # if created:
+        #     print(f'Created new record: {obj_home_pitcher}')
+        # else:
+        #     print(f'Updated existing record: {obj_home_pitcher}')
+        try:
+            obj_away_pitcher, created = Pitcher.objects.update_or_create(
+                id=away_pitcher_dic['id'],
+                defaults={ 'fullName': away_pitcher_dic['fullName'],'IP': away_pitcher_dic['IP'],
+                    'Win': away_pitcher_dic['Win'], 'Loss': away_pitcher_dic['Loss'], 'ERA': away_pitcher_dic['ERA'],
+                    'xERA': away_pitcher_dic['xERA'], 'xFIPm': away_pitcher_dic['xFIPm']
+                }
+            )
+        except:
+            print('away not created', away_pitcher_dic)
+        # if created:
+        #     print(f'Created new record: {obj_away_pitcher}')
+        # else:
+        #     print(f'Updated existing record: {obj_away_pitcher}')
+        obj_away_team, created = Team.objects.update_or_create(
+            id=away_team_dic['team_id'],
+            defaults={'team_abbrev': away_team_dic['team_abbrev'], 'name': away_team_dic['team_name'],
+                'wins': away_team_dic['win'], 'losses': away_team_dic['loss'], 'pct': away_team_dic['win_pct'],
+                'OPS': away_team_dic['OPS'], 'wOBA': away_team_dic['wOBA'], 'wRCp': away_team_dic['wRCp']
+            }
+        )
+        obj_home_team, created = Team.objects.update_or_create(
+            id=home_team_dic['team_id'],
+            defaults={'team_abbrev': home_team_dic['team_abbrev'], 'name': home_team_dic['team_name'],
+                'wins': home_team_dic['win'], 'losses': home_team_dic['loss'], 'pct': home_team_dic['win_pct'],
+                'OPS': home_team_dic['OPS'], 'wOBA': home_team_dic['wOBA'], 'wRCp': home_team_dic['wRCp']
+            }
+        )
+        obj_game, created = Game.objects.update_or_create(
+            game_id=gamethatday[i]['gamePk'],
+            defaults={'game_date': gamethatday[i]['officialDate'],
+                'away_pitcher_id': obj_away_pitcher, 'home_pitcher_id': obj_home_pitcher,
+                'away_team_id': obj_away_team, 'home_team_id': obj_home_team,
+                'home_team_score': home_team_dic['score'], 'away_team_score': away_team_dic['score'],
+            }
+        )
+        
+        # if created:
+        #     print(f'Created new record: {obj}')
+        # else:
+        #     print(f'Updated existing record: {obj}')
+    # print("in getdata: ",gamethatday)
     # print("test view")
     return gamethatday
--- gameday/models.py
+++ gameday/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
 # Create your models here.
+
+# Need to Learn how to use multiple constructors
+# and need to know how to make a custom Field
+class Team(models.Model):
+    name = models.CharField(max_length=64, default="NullTeam") # from mlb api
+    id = models.IntegerField(default=0, primary_key=True) 
+    # score = models.IntegerField(default=0) # mlb api
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    pct = models.DecimalField(default=0, decimal_places=3, max_digits=5)
+    OPS = models.DecimalField(default=0, decimal_places=3, max_digits=5)
+    wRCp = models.IntegerField(default=0)
+    wOBA = models.DecimalField(default=0, decimal_places=3, max_digits=5)
+    team_abbrev = models.CharField(max_length=16, default="NullAbbrev")
+class Pitcher(models.Model):
+    fullName = models.CharField(max_length=64, default="TBD") # mlb-api
+    id = models.IntegerField(default=0, primary_key=True)
+    IP = models.DecimalField(default=0, decimal_places=0, max_digits=5) # from BRef
+    # whip = models.DecimalField(default=0, decimal_places=2)
+    Win = models.IntegerField(default=0)
+    Loss = models.IntegerField(default=0)
+    # so9 = models.DecimalField(default=0, decimal_places=2)
+    # bb9 = models.DecimalField(default=0, decimal_places=2)
+    ERA =  models.DecimalField(default=0, decimal_places=2, max_digits=5)
+    xERA = models.DecimalField(default=0, decimal_places=2, max_digits=5) # from fanGraphs
+    xFIPm = models.IntegerField(default=0)
+    # hardhit_pct = models.DecimalField(default=0, decimal_places=3)
+    # barrel_pct = models.DecimalField(default=0, decimal_places=3)
+    def __str__(self):
+        return str(self.id) + " " + self.name
+class Game(models.Model):
+
+    game_id = models.IntegerField(primary_key=True) # PK
+    game_date = models.DateField(auto_now=False, auto_now_add=False,default="1900-01-01") # from mlb officialDate
+    
+#     away_team = Team()
+#     away_pitcher = Pitcher()
+#     home_team = Team()
+#     home_pitcher = Pitcher()
+    # away_team = models.CharField(max_length=32, default="NullTeam") # from mlb api
+    away_team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team_id', default=0)
+    away_team_score = models.IntegerField(default=0) # mlb api
+    # away_team_win = models.IntegerField(default=0)
+    # away_team_lose = models.IntegerField(default=0)
+    # away_team_pct = models.DecimalField(default=0, decimal_places=3)
+    # away_team_ops = models.DecimalField(default=0, decimal_places=3)
+    # away_team_wrcp = models.IntegerField(default=0)
+    # away_team_woba = models.DecimalField(default=0, decimal_places=3)
+
+    # away_pitcher = models.CharField(max_length=64, default="TBD") # mlb-api
+    away_pitcher_id = models.ForeignKey(Pitcher, on_delete=models.CASCADE, related_name='away_pitcher_id', default=-1) # mlb ID
+#     # away_pitcher_ip = models.DecimalField(default=0, decimal_places=0) # from BRef
+#     # away_pitcher_whip = models.DecimalField(default=0, decimal_places=2)
+#     # away_pitcher_win = models.IntegerField(default=0)
+#     # away_pitcher_lose = models.IntegerField(default=0)
+#     # away_pitcher_so9 = models.DecimalField(default=0, decimal_places=2)
+#     # # away_pitcher_bb9 = models.DecimalField(default=0, decimal_places=2)
+#     # away_pitcher_xera = models.DecimalField(default=0, decimal_places=2) # from fanGraphs
+#     # away_pitcher_xfipm = models.IntegerField(default=0)
+#     # away_pitcher_hardhitpct = models.DecimalField(default=0, decimal_places=3)
+#     # away_pitcher_barrelpct = models.DecimalField(default=0, decimal_places=3)
+
+    # home_team = models.CharField(max_length=32, default="NullTeam")
+    home_team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team_id', default=0)
+    home_team_score = models.IntegerField(default=0)
+#     # home_team_ops = models.DecimalField(default=0, decimal_places=3)
+    
+    # home_pitcher = models.CharField(max_length=64)
+    home_pitcher_id = models.ForeignKey(Pitcher, on_delete=models.CASCADE, related_name='home_pitcher_id', default=-1) # mlb ID
+#     # home_pitcher_ip = models.DecimalField(decimal_places=1)
+#     # home_pitcher_xera = models.DecimalField(decimal_places=2)
+#     # home_pitcher_xfipm = models.IntegerField(default=0)
+#     date_created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.game_id)
+
